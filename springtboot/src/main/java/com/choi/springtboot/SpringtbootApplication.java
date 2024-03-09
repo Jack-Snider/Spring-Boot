65220a9d3775e3a48278228eded8b99aafd020a0
= <choiman2021@gmail.com>
2024-03-09 17:11:41 +0900

    project made and built

---
+++ springtboot/src/main/java/com/choi/springtboot/SpringtbootApplication.java
@@ -0,0 +1,13 @@
+package com.choi.springtboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtbootApplication.class, args);
+	}
+
+}
---
+++ springtboot/src/test/java/com/choi/springtboot/SpringtbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi.springtboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
