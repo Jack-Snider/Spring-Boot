73ef893f40d70cebf2470ff7fd9685fd73132f72
= <choiman2021@gmail.com>
2024-03-16 23:32:16 +0900

    HelloController edited

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,7 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +21,16 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloSpring(@RequestParam("name") String name){
+        /*
+        ResponseBody : HTTP 응답 body안에 데이터를 직접 넣어주는 의미
+        view가 따로 없음. 그냥 데이터 자체만 있음.
+        * */
+        return "hello" + name;
+    }
+
 
 }
