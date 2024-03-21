0548bfe25d5ac87437a704f1a570b724abe43ef6
= <choiman2021@gmail.com>
2024-03-22 07:33:45 +0900

    MemberRepository edited

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,4 +1,20 @@
 package hello.hellospring.repository;
 
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberRepository {
+
+    Member save(Member member);
+
+    /*
+    from java8
+    Optional returns null if there is no result
+    * */
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
 }
